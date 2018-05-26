dfa39fb239aa374d48f8d1533798423637decb93
7rouz <harzi.rafik@gmail.com>
2018-05-26 18:30:10 +0000

    update script
    add execution rights
    fix wifi_ip variable issue

--- config_wifi_script.sh
+++ config_wifi_script.sh
@@ -4,7 +4,7 @@ set -xe
 SSID="$1"
 WIFI_PASSWORD="$2"
 
-sudo apt-get update && sudo apt-get upgarde -y 
+sudo apt-get update && sudo apt-get upgrade -y 
 
 # Update /etc/wpa_supplicant/wpa_supplicant.conf
 sudo bash -c " echo -e 'network={\n ssid=\"$SSID\" \n psk=\"$WIFI_PASSWORD\" \n }' >> /etc/wpa_supplicant/wpa_supplicant.conf"
@@ -20,6 +20,6 @@ sudo systemctl daemon-reload
 sudo systemctl restart dhcpcd
 
 # get the wifi ip adress 
-wifi_ip = $(sudo ifconfig wlan0 | grep -e "inet " | awk '{print$2}')
+wifi_ip=$(sudo ifconfig wlan0 | grep -e "inet " | awk '{print$2}')
 
 echo "Congratularions your RPI is now connected to WIFI "+$SSID+" and the ip @ is: "+$wifi_ip
