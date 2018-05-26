6b869ebb47f12caef9e113c47de7dd1f8b42469b
Rafik HARZI <7rouz@users.noreply.github.com>
2018-05-26 17:09:35 +0200

    Create config_wifi_script.sh

---
+++ config_wifi_script.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+SSID="$1"
+WIFI_PASSWORD="$2"
+
+sudo apt-get update && sudo apt-get upgarde -y 
+
+# Update /etc/wpa_supplicant/wpa_supplicant.conf
+sudo bash -c " echo -e 'network={\n ssid=\"$SSID\" \n psk=\"$WIFI_PASSWORD\" \n }' >> /etc/wpa_supplicant/wpa_supplicant.conf"
+
+# Restart WLAN0 interface
+sudo ifconfig wlan0 down
+sudo ifconfig wlan0 up
+
+# get the wifi ip adress 
+wifi_ip = $(sudo ifconfig wlan0 | grep -e "inet " | awk '{print$2}')
+
+echo "Congratularions your RPI is now connected to WIFI "+$SSID+" and the ip @ is: "+$wifi_ip
