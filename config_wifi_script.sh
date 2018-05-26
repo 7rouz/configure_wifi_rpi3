c1f37a12807a29b6add8bd3b8c9bbc68a216e413
7rouz <harzi.rafik@gmail.com>
2018-05-26 18:17:17 +0000

    update script
    reload dhcpd
    exit when a command doesn't work

--- config_wifi_script.sh
+++ config_wifi_script.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+set -xe
 
 SSID="$1"
 WIFI_PASSWORD="$2"
@@ -8,9 +9,15 @@ sudo apt-get update && sudo apt-get upgarde -y
 # Update /etc/wpa_supplicant/wpa_supplicant.conf
 sudo bash -c " echo -e 'network={\n ssid=\"$SSID\" \n psk=\"$WIFI_PASSWORD\" \n }' >> /etc/wpa_supplicant/wpa_supplicant.conf"
 
+# Restart Networking service 
+sudo service networking restart
+
 # Restart WLAN0 interface
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 up
+sudo ifconfig wlan0 down && sudo ifconfig wlan0 up
+
+# Restart dhcpd
+sudo systemctl daemon-reload
+sudo systemctl restart dhcpcd
 
 # get the wifi ip adress 
 wifi_ip = $(sudo ifconfig wlan0 | grep -e "inet " | awk '{print$2}')
